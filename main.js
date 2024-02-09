68b00bcc55ef52ca63679c1a2082bff62a38c769
TheLudde235 <lagerstrom@theludde235.se>
2024-02-09 07:19:33 +0100

    Styling and button for clearing localStorage

--- main.js
+++ main.js
@@ -1,17 +1,39 @@
 import QrScanner from "./lib/nimiq-qrcode.js";
+import { qrCodes } from "./codes.js";
+import coins from "./coinThings.js";
 
 const debugElement = document.querySelector("#debug");
 const videoElement = document.querySelector("#qr-scanner");
-const dataElement = document.querySelector('#data');
+const dataElement = document.querySelector("#data");
+const coinsElement = document.querySelector("#coins");
 
 function debug(text) {
-  const p = document.createElement("p");
-  p.textContent = JSON.stringify(text);
-  debugElement.appendChild(p);
+    const p = document.createElement("p");
+    p.textContent = JSON.stringify(text);
+    debugElement.appendChild(p);
 }
 
-const qrScanner = new QrScanner(videoElement, res => {dataElement.textContent = `Output: ${res.data}`; qrScanner.pause();}, { returnDetailedScanResult: true, highlightCodeOutline: true });
-videoElement.insertAdjacentElement('afterend', qrScanner.$canvas)
-qrScanner.$canvas.style.display = 'block'
-qrScanner.start()
+const scannedCodes = JSON.parse(localStorage.getItem("")) || qrCodes;
 
+for (const key in scannedCodes) {
+    console.log(key);
+    coins.amount += scannedCodes[key];
+}
+
+const qrScanner = new QrScanner(
+    videoElement,
+    (res) => {
+        dataElement.textContent = `Output: ${res.data}`;
+        if (alreadyScannedCodes[res.data]) {
+            dataElement.textContent = `Output: ${res.data}`;
+            localStorage.setItem(res.data, "true");
+            coinsElement.textContent++;
+        }
+    },
+    { returnDetailedScanResult: true }
+);
+
+videoElement.insertAdjacentElement("afterend", qrScanner.$canvas);
+qrScanner.$canvas.style.display = "block";
+
+qrScanner.start();
